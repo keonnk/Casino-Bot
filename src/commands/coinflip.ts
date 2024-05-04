4096db097f904c84ab7d961c1689080aee3d793a
Keon <keonk87@icloud.com>
2024-05-03 22:09:50 -0400

    created coinflip command

---
+++ src/commands/coinflip.ts
@@ -0,0 +1,45 @@
+import { ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder } from 'discord.js'
+import { SlashCommand } from '../types/types.js'
+
+const pingCommand: SlashCommand = {
+    //@ts-ignore
+    data: new SlashCommandBuilder()
+        .setName('coinflip')
+        .setDescription('play coinflip with the dealer')
+        .addStringOption((option) => {
+            return option.setName('side')
+                .setDescription('Heads or Tails')
+                .setRequired(true)
+                .addChoices(
+                    { name: 'heads', value: 'heads' },
+                    { name: 'tails', value: 'tails' }
+                )
+        }),
+    async execute(interaction: ChatInputCommandInteraction) {
+        const chosenSide = interaction.options.getString('side')
+        
+        const flippedSide = flipCoin()
+
+        let userWon: boolean = false
+        if(chosenSide == flippedSide) userWon = true;
+
+        const embedResponse = new EmbedBuilder()
+            .setColor(userWon ? 'Green' : 'Red')
+            .setTitle(userWon ? 'You won! :grin:' : 'You lost :slight_frown:')
+            .setDescription(`Coin landed on ${flippedSide}`)
+        
+        interaction.reply({embeds: [embedResponse]})
+    }
+}
+
+const flipCoin = () => {
+    const r = (Math.random() * 101) //Generates a random integer between 0 and 100
+    if (r <= 49){
+        return 'heads' //True is heads
+    }
+    else{
+        return 'tails' //False (integer between 50-100) is tails
+    }
+}
+
+export default pingCommand;
