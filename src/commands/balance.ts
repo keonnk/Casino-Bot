6c73be396a35289557664f0b5ea168d05c03af67
Keon <keonk87@icloud.com>
2024-08-17 14:58:06 -0400

    added embed for balance command

--- src/commands/balance.ts
+++ src/commands/balance.ts
@@ -1,23 +1,31 @@
-import { ChatInputCommandInteraction, ErrorEvent, SlashCommandBuilder } from 'discord.js'
-import { SlashCommand } from '../types/types.js'
-import { getUser } from '../drizzle/controllers/UserController.js'
+import {
+  ChatInputCommandInteraction,
+  EmbedBuilder,
+  SlashCommandBuilder,
+} from "discord.js";
+import { SlashCommand } from "../types/types.js";
+import { getUser } from "../drizzle/controllers/UserController.js";
 
 const balanceCommand: SlashCommand = {
-    data: new SlashCommandBuilder()
-        .setName('balance')
-        .setDescription('check your balance'),
-    async execute(interaction: ChatInputCommandInteraction) {
-        const user_id = interaction.user.id
-        try {
-            const user = await getUser(user_id)
-            await interaction.reply(`Your balance is $${user.balance}`)
-        }
-        catch (err) {
-            console.error(err)
-            await interaction.reply("ERROR: " + err.message ?? err)
-        }
+  data: new SlashCommandBuilder()
+    .setName("balance")
+    .setDescription("check your balance"),
+  async execute(interaction: ChatInputCommandInteraction) {
+    const user_id = interaction.user.id;
 
+    const embedResponse = new EmbedBuilder()
+      .setTitle("Balance")
+      .setColor("Green");
+
+    try {
+      const user = await getUser(user_id);
+      embedResponse.setDescription(`You currently have $${user.balance}`);
+      await interaction.reply({ embeds: [embedResponse] });
+    } catch (err) {
+      console.error(err);
+      await interaction.reply("ERROR: " + err.message ?? err);
     }
-}
+  },
+};
 
 export default balanceCommand;
