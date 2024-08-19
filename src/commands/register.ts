b5d34459826f2bae9035529eaf6e1ecbe3066b9d
Keon <keonk87@icloud.com>
2024-08-18 20:42:19 -0400

    added embed response and error embed to register command

--- src/commands/register.ts
+++ src/commands/register.ts
@@ -1,23 +1,36 @@
-import { ChatInputCommandInteraction, ErrorEvent, SlashCommandBuilder } from 'discord.js'
-import { SlashCommand } from '../types/types.js'
-import { createUser } from '../drizzle/controllers/UserController.js'
+import {
+  ChatInputCommandInteraction,
+  EmbedBuilder,
+  SlashCommandBuilder,
+} from "discord.js";
+import { SlashCommand } from "../types/types.js";
+import { createUser } from "../drizzle/controllers/UserController.js";
 
 const registerCommand: SlashCommand = {
-    data: new SlashCommandBuilder()
-        .setName('register')
-        .setDescription('register your user and recieve initial currency'),
-    async execute(interaction: ChatInputCommandInteraction) {
-        const user_id = interaction.user.id
-        try {
-            await createUser(user_id)
-            await interaction.reply("User successfully registered! Your balance is $1000")
-        }
-        catch (err) {
-            console.error(err)
-            await interaction.reply("ERROR: " + err.message ?? err)
-        }
+  data: new SlashCommandBuilder()
+    .setName("register")
+    .setDescription("register your user and recieve initial currency"),
+  async execute(interaction: ChatInputCommandInteraction) {
+    const user_id = interaction.user.id;
 
+    const embedResponse = new EmbedBuilder();
+
+    try {
+      await createUser(user_id);
+      embedResponse
+        .setTitle("User successfully registered!")
+        .setColor("Green")
+        .setDescription("Your balance is **$1000**");
+      await interaction.reply({ embeds: [embedResponse] });
+    } catch (err) {
+      console.error(err);
+      const errorEmbed = new EmbedBuilder()
+        .setTitle("ERROR")
+        .setColor("Red")
+        .setDescription(err.message ?? err);
+      await interaction.reply({ embeds: [errorEmbed] });
     }
-}
+  },
+};
 
 export default registerCommand;
